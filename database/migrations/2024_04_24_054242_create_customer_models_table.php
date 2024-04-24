c5ea1d5122e7ab34f33230f7bd79e97460cd7871
Tarek <tarek@tedybusiness.com>
2024-04-24 08:36:47 +0200

    he llenado el order_model con los campos que me parecen fundamentales para crear un order de prueba

--- database/migrations/2024_04_24_054242_create_customer_models_table.php
+++ database/migrations/2024_04_24_054242_create_customer_models_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('customer_models', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
-            $table->string('last_name');
+            $table->string('last_name')->nullable();
             $table->string('customer_type');
             $table->string('email');
             $table->string('password');
