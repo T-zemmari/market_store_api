dc5710076cb5cdb974b9fad38533f620e2225c17
Tarek <tarek@tedybusiness.com>
2024-05-13 11:16:39 +0200

    vista conacto con un formulario de prueba

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,83 @@
 <x-guest-layout>
     @section('title', 'Market Store - Contacto')
     @include('layouts.partials.navbar')
+
+    <div class="w-full flex justify-center items-center h-[92vh]">
+        <div class="max-w-screen-md mx-auto p-5 mt-[150px]">
+            <div class="text-center mb-2">
+                <p class="mt-4 text-sm leading-7 text-gray-500 font-regular uppercase">
+                    Contactanos
+                </p>
+                <h3 class="text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight text-gray-900">
+                    Rellenando <span class="text-indigo-600">El Formulario</span>
+                </h3>
+            </div>
+
+            <form class="w-full">
+                <div class="flex flex-wrap -mx-3 mb-6">
+                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                            for="grid-first-name">
+                            Nombre
+                        </label>
+                        <input
+                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                            id="grid-first-name" type="text" placeholder="Nombre">
+                        <p class="text-red-500 text-xs italic">El campos es requerido.</p>
+                    </div>
+                    <div class="w-full md:w-1/2 px-3">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                            for="grid-last-name">
+                            Apellidos
+                        </label>
+                        <input
+                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="grid-last-name" type="text" placeholder="Apellidos">
+                    </div>
+                </div>
+                <div class="flex flex-wrap -mx-3 mb-6">
+                    <div class="w-full px-3">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                            for="grid-email">
+                            Email
+                        </label>
+                        <input
+                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="grid-email" type="email" placeholder="********@*****.**">
+                    </div>
+                </div>
+
+                <div class="flex flex-wrap -mx-3 mb-6">
+                    <div class="w-full px-3">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                            for="grid-password">
+                            Mensaje
+                        </label>
+                        <textarea rows="10"
+                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                  
+                </textarea>
+                    </div>
+                    <div class="flex justify-between w-full px-3">
+                        <div class="md:flex md:items-center">
+                            <label class="block text-gray-500 font-bold">
+                                <input class="mr-2 leading-tight" type="checkbox">
+                                <span class="text-sm">
+                                    Registrame en tu newsletter
+                                </span>
+                            </label>
+                        </div>
+                        <button
+                            class="shadow bg-indigo-600 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded"
+                            type="submit">
+                            Enviar mensaje
+                        </button>
+                    </div>
+
+                </div>
+
+            </form>
+        </div>
+
+    </div>
 </x-guest-layout>
