ce4726160dad9dd5447784613b4e575a9b8dcf00
Tarek <tarek@tedybusiness.com>
2024-05-30 09:14:04 +0200

    nuevo vid demo

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -2,13 +2,16 @@
     @section('title', 'Market Store API - Página de inicio')
     @include('layouts.partials.navbar')
 
-    <section class="h-[92.6vh]" style="background: radial-gradient(circle at 24.1% 68.8%, rgb(50, 50, 50) 0%, rgb(0, 0, 0) 99.4%);">
+    <section class="h-[92.6vh]"
+        style="background: radial-gradient(circle at 24.1% 68.8%, rgb(50, 50, 50) 0%, rgb(0, 0, 0) 99.4%);">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12  ">
             <div class="mr-auto place-self-center lg:col-span-5 mt-[145px]">
                 <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl text-white">
                     API de Market Store</h1>
-                <p class="max-w-2xl mb-6 font-light text-white lg:mb-8 md:text-lg lg:text-xl">Bienvenido a la demo de la API de Market Store. Esta página te permite realizar peticiones a la API para obtener información sobre productos, clientes, categorías y pedidos.</p>
-                <a href="{{route('documentacion')}}"
+                <p class="max-w-2xl mb-6 font-light text-white lg:mb-8 md:text-lg lg:text-xl">Bienvenido a la demo de la
+                    API de Market Store. Esta página te permite realizar peticiones a la API para obtener información
+                    sobre productos, clientes, categorías y pedidos.</p>
+                <a href="{{ route('documentacion') }}"
                     class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300">
                     Documentación
                     <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
@@ -20,8 +23,8 @@
                 </a>
 
             </div>
-            <div class="hidden  lg:col-span-7 lg:flex lg:mt-[200px]">
-                <img src="{{asset('assets/vids/api_store.gif')}}" alt="gif_1">
+            <div class="hidden lg:col-span-7 lg:flex lg:mt-[200px]">
+                <video src="{{ asset('assets/vids/mst_1.mp4') }}" autoplay loop muted></video>
             </div>
         </div>
     </section>
